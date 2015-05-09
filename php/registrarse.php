091e5199584892e33e1ccb191ed5a2e0bab78716
Jogua <josegua93@gmail.com>
2015-05-09 17:57:08 +0200

    registrarse

--- php/registrarse.php
+++ php/registrarse.php
@@ -4,10 +4,10 @@ include './conexion_bd.php';
 session_start();
 
 $consulta = 'INSERT INTO usuario (nombre_usuario, password, mail, tipo_usuario) VALUES ("'
-        . $_POST['nombre'] . '", "'
+        . $_POST['nombre_usuario'] . '", "'
         . $_POST['contrasena2'] . '", "'
         . $_POST['mail'] . '", "'
-        . $_POST['tipoUsuario'] . '") ';
+        . $_POST['tipo_usuario'] . '") ';
 
 $resultado = conexionBD($consulta);
 
@@ -19,6 +19,10 @@ if (!$resultado) {
 } else {
     $_SESSION['nombre_usario'] = $_POST['nombre_usuario'];
     $_SESSION['tipo_usuario'] = $_POST['tipo_usuario'];
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
+//    header('Location: ' . $_SERVER['HTTP_REFERER']);
+    echo '<script>
+            alert("Exito");
+            location.href= " ' . $_SERVER['HTTP_REFERER'] . '";
+        </script>';
 }
 ?>
