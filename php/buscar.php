b309f110dfaacae98405e080d901c5b3c4850844
Jogua <josegua93@gmail.com>
2015-05-09 20:28:46 +0200

    intento de insertar alojamiento

---
+++ php/buscar.php
@@ -0,0 +1,27 @@
+<?php
+include './conexion_bd.php';
+session_start();
+
+ESTO NO HACE NAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+$consulta = 'SELECT INTO usuario (nombre_usuario, password, mail, tipo_usuario) VALUES ("'
+        . $_POST['nombre_usuario'] . '", "'
+        . $_POST['contrasena2'] . '", "'
+        . $_POST['mail'] . '", "'
+        . $_POST['tipo_usuario'] . '") ';
+
+$resultado = conexionBD($consulta);
+
+if (!$resultado) {
+    echo '<script>
+            alert("El usuario ya existe.");
+            location.href= " ' . $_SERVER['HTTP_REFERER'] . '";
+        </script>';
+} else {
+    $_SESSION['nombre_usuario'] = $_POST['nombre_usuario'];
+    $_SESSION['tipo_usuario'] = $_POST['tipo_usuario'];
+    echo '<script>
+            location.href= " ' . $_SERVER['HTTP_REFERER'] . '";
+        </script>';
+}
+?>
